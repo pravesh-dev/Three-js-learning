7b42c6472e8cb1f2db9820889c48359a68ef6135
pravesh dav <humantorch1932@gmail.com>
2024-09-21 17:22:27 +0530

    draw a cube

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
+scene.add(camera);
 
-const myConvas = document.querySelector("#my-canvas");
+let box = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({ color: 'teal' });
+let mesh = new THREE.Mesh(box, material);
 
-const renderer = new THREE.WebGLRenderer({ canvas: myConvas });
+scene.add(mesh);
+
+const canvas = document.querySelector("#my-canvas");
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-function animate() {
-    window.requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-}
-animate();
+renderer.render(scene, camera);
