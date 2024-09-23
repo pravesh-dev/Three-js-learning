e978fc04c3125e76cb49c0c8c151a3c8af231237
pravesh dav <humantorch1932@gmail.com>
2024-09-23 15:45:08 +0530

    added orbit controls to give mouse controls to cube, like rotating using mouse zoom in and out

--- Three_bundle/main.js
+++ Three_bundle/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -9,7 +10,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 'teal' });
+const material = new THREE.MeshBasicMaterial({ color: 'teal', wireframe: true });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -25,9 +26,12 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
 })
 
+const controls = new OrbitControls( camera, renderer.domElement );
+
 function animate() {
   window.requestAnimationFrame(animate);
   renderer.render(scene, camera);
+  controls.update();
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 }
