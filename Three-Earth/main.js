c0494705ffe666a04dde59c567f81d62068adfe8
pravesh dav <humantorch1932@gmail.com>
2024-09-25 18:18:56 +0530

    many fixes in main js of threearth and added some more textures

--- Three-Earth/main.js
+++ Three-Earth/main.js
@@ -6,14 +6,26 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 5;
 // scene.add(camera)
+let canvas = document.querySelector('.my-canvas')
+const renderer = new THREE.WebGLRenderer({canvas, antialias: true})
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 
 const earthGroup = new THREE.Group();
 earthGroup.rotation.z = -23.4 * Math.PI / 180;
 scene.add(earthGroup);
 
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
 const textureLoader = new THREE.TextureLoader();
 let geometry = new THREE.IcosahedronGeometry(1.8, 20)
-let material = new THREE.MeshStandardMaterial({ map: textureLoader.load('/earth.jpg') })
+let material = new THREE.MeshStandardMaterial({ map: textureLoader.load('/earth.jpg'), specularMap: textureLoader.load("/02_earthspec1k.jpg"),
+  bumpMap: textureLoader.load("/01_earthbump1k.jpg"),
+  bumpScale: 0.04, })
 // let material = new THREE.MeshStandardMaterial({ map: textureLoader.load('/some.jpg') })
 
 let earthMesh = new THREE.Mesh(geometry, material)
@@ -23,7 +35,7 @@ let lightMaterial = new THREE.MeshBasicMaterial({ map: textureLoader.load('/eart
 let lightMesh = new THREE.Mesh(geometry, lightMaterial);
 earthGroup.add(lightMesh);
 
-let cloudMaterial = new THREE.MeshStandardMaterial({ map: textureLoader.load('/cloud.jpg'), blending: THREE.AdditiveBlending })
+let cloudMaterial = new THREE.MeshStandardMaterial({ map: textureLoader.load('/cloud.jpg'), blending: THREE.AdditiveBlending, alphaMap: textureLoader.load('/05_earthcloudmaptrans.jpg'),})
 let cloudMesh = new THREE.Mesh(geometry, cloudMaterial);
 cloudMesh.scale.setScalar(1.006)
 earthGroup.add(cloudMesh);
@@ -31,38 +43,31 @@ earthGroup.add(cloudMesh);
 let stars = getStarField({numStars: 3000});
 scene.add(stars)
 
-const sunLight = new THREE.DirectionalLight(0xffffff, 0.3);
+const sunLight = new THREE.DirectionalLight(0xffffff, 2.0);
 sunLight.position.set(-2, 0.5, 1.5);
 scene.add(sunLight)
 
-let canvas = document.querySelector('.my-canvas')
-const renderer = new THREE.WebGLRenderer({canvas, antialias: true})
-renderer.setSize(window.innerWidth, window.innerHeight)
-
-window.addEventListener("resize", () => {
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-})
-
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.enableDamping = true;
-controls.dampingFactor = 0.05;
 
 let clock = new THREE.Clock()
 
-
 function animate() {
   requestAnimationFrame(animate)
-  renderer.render(scene, camera)
-
+  
   let time = clock.getElapsedTime()
   earthMesh.rotation.y = time * 0.04;
   lightMesh.rotation.y = time * 0.04;
   cloudMesh.rotation.y = time * 0.1;
   stars.rotation.y = -(time * 0.01);
-
+  
+  renderer.render(scene, camera)
   controls.update()
 }
 
-animate()
+animate();
+
+
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}, false)
