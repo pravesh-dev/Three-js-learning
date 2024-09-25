2267714951f0dd27076963adc1996fb06af9d7b8
pravesh dav <humantorch1932@gmail.com>
2024-09-25 13:54:24 +0530

    some changes

--- Three_bundle/main.js
+++ Three_bundle/main.js
@@ -10,7 +10,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 // const geometry = new THREE.SphereGeometry(2, 32, 16, 1, Math.PI * 2, 0, Math.PI * 4);
-const geometry = new THREE.IcosahedronGeometry(1, 2);
+const geometry = new THREE.IcosahedronGeometry(2.4, 2);
 const material = new THREE.MeshStandardMaterial({flatShading: true, color: '0xffffff'});
 const wireMaterial = new THREE.MeshBasicMaterial({color: '0xffffff', wireframe: true, antialias: true});
 const cube = new THREE.Mesh(geometry, material);
@@ -18,28 +18,28 @@ const wireMesh = new THREE.Mesh(geometry, wireMaterial);
 scene.add(cube);
 cube.add(wireMesh);
 
-// let hemilight = new THREE.HemisphereLight('salmon', 'cyan', 0.9);
-// const sphereHelper = new THREE.HemisphereLightHelper( hemilight, 3 );
-// scene.add(hemilight);
+let hemilight = new THREE.HemisphereLight('salmon', 'cyan', 0.9);
+const sphereHelper = new THREE.HemisphereLightHelper( hemilight, 3 );
+scene.add(hemilight);
 // scene.add(sphereHelper);
 
-// const light = new THREE.DirectionalLight( 'green', 1 );
-// light.position.set( 3, 1, 0 );
-// const directionHelper = new THREE.DirectionalLightHelper( light, 2 );
-// scene.add( light );
+const light = new THREE.DirectionalLight( 'green', 1 );
+light.position.set( 3, 1, 0 );
+const directionHelper = new THREE.DirectionalLightHelper( light, 2 );
+scene.add( light );
 // scene.add( directionHelper );
 
-// const pointLight = new THREE.PointLight('salmon', 1, 100);
-// pointLight.position.set(-1, 1, 0);
-// const pointHelper = new THREE.PointLightHelper(pointLight, 1);
-// scene.add(pointLight);
+const pointLight = new THREE.PointLight('salmon', 1, 100);
+pointLight.position.set(-1, 1, 0);
+const pointHelper = new THREE.PointLightHelper(pointLight, 1);
+scene.add(pointLight);
 // scene.add(pointHelper);
 
 const spotLight = new THREE.SpotLight( 0xffffff );
 spotLight.position.set( 1, -2, 2 );
 const spotLightHelper = new THREE.SpotLightHelper( spotLight );
 scene.add( spotLight );
-scene.add( spotLightHelper );
+// scene.add( spotLightHelper );
 
 camera.position.z = 5;
 
