73b2d21a554a370c777e33b5342f7941ab352877
pravesh dav <humantorch1932@gmail.com>
2024-09-29 13:40:31 +0530

    added handlewindowresize funciton

--- WarmHole/main.js
+++ WarmHole/main.js
@@ -31,3 +31,10 @@ function animate() {
 }
 
 animate();
+
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
