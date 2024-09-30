8c38548893ff839f08e28028922fff31ae56603d
pravesh dav <humantorch1932@gmail.com>
2024-09-30 13:15:06 +0530

    some changes ub wormhole tube geometry

--- WarmHole/main.js
+++ WarmHole/main.js
@@ -1,33 +1,36 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import spline from './spline.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import spline from "./spline.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000)
-camera.position.z = 5;  
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
 
-let canvas = document.querySelector('.my-canvas')
-const renderer = new THREE.WebGLRenderer({canvas, antialias: true})
+let canvas = document.querySelector(".my-canvas");
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.outputColorSpace = THREE.SRGBColorSpace;
-renderer.setSize(w, h)
+renderer.setSize(w, h);
 
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 
-
-let tubegeo = new THREE.TubeGeometry(spline, 222, 0.65, 16, true)
-let material = new THREE.MeshBasicMaterial({ wireframe: true});
-let tube = new THREE.Mesh(tubegeo, material)
-scene.add(tube)
+let tubegeo = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
+let material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  // side: THREE.DoubleSide,
+  wireframe: true,
+});
+let tube = new THREE.Mesh(tubegeo, material);
+scene.add(tube);
 
 function animate() {
-  requestAnimationFrame(animate)
-  renderer.render(scene, camera)
-  controls.update()
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  controls.update();
 }
 
 animate();
@@ -37,4 +40,4 @@ function handleWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
-window.addEventListener('resize', handleWindowResize, false);
+window.addEventListener("resize", handleWindowResize, false);
