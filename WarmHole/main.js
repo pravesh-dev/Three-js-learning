91e4612e95558edcb2ae8e749ca0a1b1ffa82d5a
pravesh dav <humantorch1932@gmail.com>
2024-09-30 15:01:23 +0530

    removed new wormhole cause can't find the solution

--- WarmHole/main.js
+++ WarmHole/main.js
@@ -4,34 +4,37 @@ import spline from "./spline.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
-
 const scene = new THREE.Scene();
-scene.fog = new THREE.FogExp2(0x000000, 0.3);
+scene.fog = new THREE.FogExp2(0x000019, 0.3);
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
 
 let canvas = document.querySelector(".my-canvas");
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-renderer.setSize(w, h);
-renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.outputColorSpace = THREE.SRGBColorSpace;
+renderer.setSize(w, h);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 
-const tubegeo = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
-const tubeMaterial = new THREE.MeshBasicMaterial({
-  // side: THREE.DoubleSide,
+let tubegeo = new THREE.TubeGeometry(spline, 500, 0.7, 20, true);
+let material = new THREE.MeshStandardMaterial({
+  side: THREE.DoubleSide,
+});
+let material2 = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
   wireframe: true,
 });
-const tube = new THREE.Mesh(tubegeo, tubeMaterial);
-// scene.add(tube);
 
-const edges = new THREE.EdgesGeometry(tubegeo);
-const linematerial = new THREE.LineBasicMaterial({ color: 0xffffff });
-const tubeLines = new THREE.LineSegments(edges, linematerial);
-scene.add(tubeLines);
+let tube = new THREE.Mesh(tubegeo, material);
+let wireTube = new THREE.Mesh(tubegeo, material2);
+scene.add(tube);
+scene.add(wireTube);
+
+let hemiLight = new THREE.HemisphereLight(0x274156, 0x1C6E8C, 0.9);
+scene.add(hemiLight);
 
 function updateCamera(t) {
   const time = t * 0.03;
@@ -49,6 +52,7 @@ function animate(t = 0) {
   renderer.render(scene, camera);
   controls.update();
 }
+
 animate();
 
 function handleWindowResize() {
@@ -57,68 +61,3 @@ function handleWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 window.addEventListener("resize", handleWindowResize, false);
-
-
-// import * as THREE from "three";
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-// import spline from "./spline.js";
-
-// const w = window.innerWidth;
-// const h = window.innerHeight;
-// const scene = new THREE.Scene();
-// scene.fog = new THREE.FogExp2(0x000019, 0.3);
-// const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
-// camera.position.z = 5;
-
-// let canvas = document.querySelector(".my-canvas");
-// const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-// renderer.toneMapping = THREE.ACESFilmicToneMapping;
-// renderer.outputColorSpace = THREE.SRGBColorSpace;
-// renderer.setSize(w, h);
-
-// const controls = new OrbitControls(camera, renderer.domElement);
-// controls.enableDamping = true;
-// controls.dampingFactor = 0.05;
-
-// let tubegeo = new THREE.TubeGeometry(spline, 500, 0.7, 20, true);
-// let material = new THREE.MeshStandardMaterial({
-//   side: THREE.DoubleSide,
-// });
-// let material2 = new THREE.MeshBasicMaterial({
-//   color: 0xffffff,
-//   wireframe: true,
-// });
-
-// let tube = new THREE.Mesh(tubegeo, material);
-// let wireTube = new THREE.Mesh(tubegeo, material2);
-// scene.add(tube);
-// scene.add(wireTube);
-
-// let hemiLight = new THREE.HemisphereLight(0x274156, 0x1C6E8C, 0.9);
-// scene.add(hemiLight);
-
-// function updateCamera(t) {
-//   const time = t * 0.03;
-//   const looptime = 10 * 1000;
-//   const p = (time % looptime) / looptime;
-//   const pos = tubegeo.parameters.path.getPointAt(p);
-//   const lookAt = tubegeo.parameters.path.getPointAt((p + 0.01) % 1);
-//   camera.position.copy(pos);
-//   camera.lookAt(lookAt);
-// }
-
-// function animate(t = 0) {
-//   requestAnimationFrame(animate);
-//   updateCamera(t);
-//   renderer.render(scene, camera);
-//   controls.update();
-// }
-
-// animate();
-
-// function handleWindowResize() {
-//   camera.aspect = window.innerWidth / window.innerHeight;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-// }
-// window.addEventListener("resize", handleWindowResize, false);
