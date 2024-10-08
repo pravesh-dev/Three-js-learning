2a8802f22d6cc0995ad6750c17d439e58a823bf8
pravesh dav <humantorch1932@gmail.com>
2024-10-08 17:25:24 +0530

    some changes in text and removed shadow

--- Three_bundle/main.js
+++ Three_bundle/main.js
@@ -36,34 +36,35 @@ function addTextToCube(text) {
   loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function(font) {
     const textGeometry = new TextGeometry(text, {
       font: font,
-      size: 0.3,
+      size: 0.35,
       depth: 0.1,
       curveSegments: 4,
       bevelEnabled: false,
     });
-    const textMaterial = new THREE.MeshPhongMaterial({ color: 0xffff33 });
+    const textMaterial = new THREE.MeshPhongMaterial({ color: 0x2f7463 });
     const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-    textMesh.position.set(-1.2, 1.2, 1.3); // Adjust position relative to cube
+    // textMesh.position.set(-1.2, 1.2, 1.3); // Adjust position relative to cube
+    textMesh.position.set(-1.2, 0.85, 1.3); // Adjust position relative to cube
     cube.add(textMesh); // Add text as a child of the cube
 
     // Create shadow text
     const shadowGeometry = new TextGeometry(text, {
       font: font,
-      size: 0.3,
+      size: 0.35,
       depth: 0.1,
       curveSegments: 4,
       bevelEnabled: false,
     });
-    const shadowMaterial = new THREE.MeshPhongMaterial({ color: 0xffff33, opacity: 0.1, transparent: true });
+    const shadowMaterial = new THREE.MeshPhongMaterial({ color: 0x2f7463, opacity: 0.4, transparent: true });
     const shadowMesh = new THREE.Mesh(shadowGeometry, shadowMaterial);
     shadowMesh.position.set(-1.18, 1.2, 1.2); // Slightly offset from the main text and on the back
     shadowMesh.rotation.x = -1; // Rotate to face the back
-    cube.add(shadowMesh);
+    // cube.add(shadowMesh);
   });
 }
 
 // Call function to add text
-addTextToCube('metal Cube');
+addTextToCube('Metal Cube');
 
 
 camera.position.z = 5;
@@ -107,7 +108,7 @@ const meshFolder = gui.addFolder('Mesh');
 meshFolder.add(cube, 'visible');
 
 // Add hemisphere light
-const hemisphereLight = new THREE.HemisphereLight(0x504401, 0xffffff, 0.9);
+const hemisphereLight = new THREE.HemisphereLight(0x2f7463, 0xffffff, 0.9);
 // Set light position
 hemisphereLight.position.set(1.4, 1.3, 0.2);
 scene.add(hemisphereLight);
