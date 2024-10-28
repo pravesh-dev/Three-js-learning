a9e20f92710f67a8c242e36872f5f15b9465890d
pravesh dav <humantorch1932@gmail.com>
2024-10-28 21:27:46 +0530

    control model and lighting rander timing

--- Cyberpunk/main.js
+++ Cyberpunk/main.js
@@ -18,28 +18,30 @@ renderer.toneMappingExposure = 1;
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-// Load HDRI
+let helmetModel;
+const loader = new GLTFLoader();
+
+// Load HDRI first, then load the 3D model
 const rgbeLoader = new RGBELoader();
 rgbeLoader.load('https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/brown_photostudio_02_1k.hdr', function (texture) {
     texture.mapping = THREE.EquirectangularReflectionMapping;
     scene.environment = texture;
     scene.background = texture;
+    
+    // Load the 3D model after HDRI is loaded
+    loader.load(
+        '/DamagedHelmet.gltf',
+        function (gltf) {
+            helmetModel = gltf.scene;
+            helmetModel.scale.set(6, 6, 6);
+            scene.add(helmetModel);
+        },
+        undefined,
+        function (error) {
+            console.error(error);
+        }
+    );
 });
-
-let helmetModel;
-const loader = new GLTFLoader();
-loader.load(
-    '/DamagedHelmet.gltf',
-    function (gltf) {
-        helmetModel = gltf.scene;
-        helmetModel.scale.set(6, 6, 6);
-        scene.add(helmetModel);
-    },
-    undefined,
-    function (error) {
-        console.error(error);
-    }
-);
 
 const clock = new THREE.Clock();
 
