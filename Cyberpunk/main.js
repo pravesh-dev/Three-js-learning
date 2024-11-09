03a129ed7161c018329f68cac3aec903eb29c0f2
pravesh dav <humantorch1932@gmail.com>
2024-11-09 13:21:50 +0530

    created bee page folder and downloaded imaes

--- Cyberpunk/main.js
+++ Cyberpunk/main.js
@@ -10,6 +10,7 @@ import gsap from 'gsap';
 
 const scene = new THREE.Scene();
 
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 15;
 
