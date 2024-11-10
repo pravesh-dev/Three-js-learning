8d58f51484560c5d1cd1f21751a39387de7e1c8f
pravesh dav <humantorch1932@gmail.com>
2024-11-10 13:33:10 +0530

    lighting in bee page

--- BEE_PAGE/script.js
+++ BEE_PAGE/script.js
@@ -29,3 +29,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('container3D').appendChild(renderer.domElement);
 
 
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
