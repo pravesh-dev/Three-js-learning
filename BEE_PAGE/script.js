d03afb7030de43aff196ca273834e8db1dacb040
pravesh dav <humantorch1932@gmail.com>
2024-11-10 13:29:46 +0530

    created basic 3d scence and loaded bee model in bee page

---
+++ BEE_PAGE/script.js
@@ -0,0 +1,31 @@
+
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+import { gsap } from 'https://cdn.skypack.dev/gsap';
+
+const camera = new THREE.PerspectiveCamera(
+    10,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 13;
+
+const scene = new THREE.Scene();
+let bee;
+
+const loader = new GLTFLoader();
+loader.load('./model/bee_bot.glb',
+    function(gltf) {
+        bee = gltf.scene;
+        scene.add(bee)
+    },
+    function(xhr) {},
+    function(err) {}
+);
+
+const renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('container3D').appendChild(renderer.domElement);
+
+
