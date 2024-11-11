4eec2129534bac571a514041dae5b086266afa7f
pravesh dav <humantorch1932@gmail.com>
2024-11-11 17:31:53 +0530

    animated the images using gsap

--- ThirtySixStudio/src/Canvas.jsx
+++ ThirtySixStudio/src/Canvas.jsx
@@ -1,20 +1,35 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import canvasimages from './canvasimages'
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap';
 
 function canvas() {
     const canvasRef = useRef(null);
+    const [index, setIndex] = useState({value: 0});
+
+    useGSAP(()=>{
+        gsap.to(index, {
+            value: 149,
+            repeat: -1,
+            duration: 3,
+            ease: 'linear',
+            onUpdate: () => {
+                setIndex({ value: Math.round(index.value) })
+            }
+        })
+    })
 
     useEffect(() => {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext('2d');
       const img = new Image();
-      img.src = canvasimages[0];
+      img.src = canvasimages[index.value];
       img.onload = () =>{
         canvas.width = img.width;
         canvas.height = img.height;
         ctx.drawImage(img, 0, 0)
       };
-    }, [])
+    }, [index])
     
   return (
     <canvas ref={canvasRef} className='w-52 h-52' id='canvas'></canvas>
